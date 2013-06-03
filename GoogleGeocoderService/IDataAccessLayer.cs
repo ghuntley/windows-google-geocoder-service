e0730a297cd436054abc26083ae3f2f54606b343
Geoffrey Huntley <ghuntley@ghuntley.com>
2013-06-03 15:28:24 +1000

    IDataAccessLayer.GetOutstanding now is <T> of GoogleGeocoderJob

--- GoogleGeocoderService/IDataAccessLayer.cs
+++ GoogleGeocoderService/IDataAccessLayer.cs
@@ -7,11 +7,11 @@ namespace GoogleGeocoderService
         /// <summary>
         /// retrieves list of database records which require geoencoding.
         /// </summary>
-        Dictionary<int, string> GetOutstanding();
+        IList<GoogleGeocoderJob> GetOutstanding();
 
         /// <summary>
         /// updates database record with response from identified by (dictionary key) with data from response.
         /// </summary>
-        void SaveResponse(int key, GoogleGeocodeResponse response);
+        void SaveResponse(GoogleGeocoderJob job, GoogleGeocodeResponse response);
     }
 }
