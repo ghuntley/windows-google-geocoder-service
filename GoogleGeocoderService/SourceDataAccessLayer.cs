b89bbd85fa9759f256efb65c5585a31fefdd11fb
Geoffrey Huntley <ghuntley@ghuntley.com>
2013-06-03 15:29:24 +1000

    added SourceDataAccessLayer

---
+++ GoogleGeocoderService/SourceDataAccessLayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SourceDb;
+
+namespace GoogleGeocoderService
+{
+    public class SourceDataAccessLayer : IDataAccessLayer
+    {
+        private SourceDbDataContext _sourceDbDataContext; 
+ 
+        /// <summary>
+        /// retrieves list of database records which require geoencoding.
+        /// </summary>
+        public IList<GoogleGeocoderJob> GetOutstanding()
+        {
+            _sourceDbDataContext = new SourceDbDataContext(AppConfig.ConnectionString);
+
+            var query = from debtors in _sourceDbDataContext.DEBTORS
+                        where (debtors.Longitude.Equals(null) || debtors.Latitude.Equals(null))
+
+                        select new GoogleGeocoderJob()
+                        {
+                            AccountId = debtors.AccountID,
+                            Address = ((debtors.Address1 ?? "") + " " + (debtors.Address2 ?? "") + " " + (debtors.Address3 ?? "") + " " + (debtors.Address4 ?? "") + " " + (debtors.City ?? "") + " " + (debtors.State ?? "") + " " + (debtors.PostCode ?? "")).Trim(),
+                            CompanyName = debtors.Company
+                        };
+
+            return query.ToList();
+        }
+
+        /// <summary>
+        /// updates database record with response from identified by (dictionary key) with data from response.
+        /// </summary>
+        public void SaveResponse(int accountid, GoogleGeocodeResponse response)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
